c0f2b9761ac13381f02225429d34c7f89baa5230
nhatled18 <nhatled18@gmail.com>
2023-11-28 09:20:30 +0700

    Add buttons and add event handling for the buttons.

--- app/src/main/java/com/example/jobfindernew/MainActivity.java
+++ app/src/main/java/com/example/jobfindernew/MainActivity.java
@@ -2,13 +2,105 @@ package com.example.jobfindernew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private Button btnSearch;
+    private Button btnPost;
+    private Button btnManage;
+    private Button btnApply;
+    private Button btnNotification;
+    private Button btnAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        btnSearch = findViewById(R.id.btn_search);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        btnPost = findViewById(R.id.btn_post);
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PostActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        btnManage = findViewById(R.id.btn_manage);
+        btnManage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ManageActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        btnApply = findViewById(R.id.btn_apply);
+        btnApply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ApplyActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        btnNotification = findViewById(R.id.btn_notification);
+        btnNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NotificationActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        btnAccount = findViewById(R.id.btn_account);
+        btnAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        btnAccount = findViewById(R.id.btn_account);
+        btnAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
